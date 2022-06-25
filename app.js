a3f95b9a14e9da69ef16ddeb347eae988ae352a5
GMuskan <55504711+GMuskan@users.noreply.github.com>
2022-06-25 13:11:42 +0530

    button and input

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+console.log("clicked");
+function clickEventHandler(){
+    console.log("clicked");
+    console.log("input", txtInput.value);
+ }
+btnTranslate.addEventListener("click",  clickEventHandler);
+  
